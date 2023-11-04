639b4993b50054b44399b5bae10cfe755ec1b54a
Rogerskelamen <rogerkelamen@gmail.com>
2023-11-04 20:39:00 +0800

    finish argument handling

--- risas.c
+++ risas.c
@@ -12,6 +12,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define FILE_RD  "r"
 #define FILE_WRT "w"
@@ -30,9 +31,19 @@ static FILE *fp; // file pointer to source file
 static int out_fmt; // output file format
 
 static void
+usage_fmt()
+{
+  printf("==== protential output file format ====\n");
+  printf("- 1: hexadecimal machine code(one byte per line)\n");
+  printf("- 2: hexadecimal machine code(one instruction per line)\n");
+  printf("- 3: binary machine code(instruction per line)\n");
+}
+
+static void
 usage(char *cmd)
 {
-  printf("Usage: %s [riscv assembly file] -f [output file format]\n", cmd);
+  printf("Usage: %s [riscv assembly file] -f[output file format]\n", cmd);
+  usage_fmt();
 }
 
 static int
@@ -40,13 +51,10 @@ get_fmt()
 {
   int f;
   printf("choose one of the output formats:\n");
-  printf("1. hexadecimal machine code(one byte per line)\n");
-  printf("2. hexadecimal machine code(one instruction per line)\n");
-  printf("3. binary machine code(instruction per line)\n");
+  usage_fmt();
   if ((f = getc(stdin)) == EOF || f - '0' < 1 || f - '0' > 3) {
     return 0;
   }
-
   return f - '0';
 }
 
@@ -63,20 +71,49 @@ int main(int argc, char *argv[])
     fprintf(stderr, "%s: can't open %s, please make sure the pid you typed exists\n", argv[0], argv[1]);
     exit(ERR_FILE);
   }
-  // determine output file format
-  if (argc <= 3) {
+  // determine output file format when command is not complete
+  if (argc <= 2) {
     if(!(out_fmt = get_fmt())) {
-      fprintf(stderr, "%s: the option you typed is incorrect\n", argv[1]);
+      fprintf(stderr, "%s: the option you typed in is incorrect\n", argv[0]);
+      fclose(fp);
       exit(ERR_ARG);
     }
   }
-  printf("the choice is %d", out_fmt);
+  if (argc > 3 || strlen(argv[2]) != 3 || *argv[2] != '-' || *(argv[2] + 1) != 'f') {
+    usage(argv[0]);
+    fclose(fp);
+    exit(ERR_ARG);
+  }
+
+  // the command is entered correctly
+  // extract from the arguments
+  switch (*(argv[2] + 2)) {
+    case '1':
+      out_fmt = OUT_HEXB;
+      break;
+    case '2':
+      out_fmt = OUT_HEX;
+      break;
+    case '3':
+      out_fmt = OUT_BIN;
+      break;
+    default:
+      fprintf(stderr, "%s: the argument you typed in is incorrect\n", argv[0]);
+      fclose(fp);
+      exit(ERR_ARG);
+      break;
+  }
+
+  // printf("the choice is %d\n", out_fmt);
 
   // 1. first traverse to get all tags
 
   // input from stdin
   // if (argc > 2) {
   // }
+
+  // Clean up
+  fclose(fp);
 
   return EXIT_SUCCESS;
 }
